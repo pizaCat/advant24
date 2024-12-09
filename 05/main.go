1b5ece0cd9090c928edac6ae1b7bc579e04232c7
pizaCat <30298207+pizaCat@users.noreply.github.com>
2024-12-09 03:15:19 +0000

    day 05

--- 05/main.go
+++ 05/main.go
@@ -24,6 +24,7 @@ func main() {
 	}
 
 	result := 0
+	result_p2 := 0
 	for _, line := range update_lines {
 		update := []int{}
 		for _, s := range strings.Split(line, ",") {
@@ -33,16 +34,21 @@ func main() {
 		if in_order(update, rules) {
 			middle_i := (len(update) - 1) / 2
 			result = result + update[middle_i]
+		} else {
+			update = order_update(update, rules)
+			middle_i := (len(update) - 1) / 2
+			result_p2 = result_p2 + update[middle_i]
 		}
 	}
 
 	fmt.Println("Part1: ", result)
+	fmt.Println("Part1: ", result_p2)
 }
 
-func in_order(seq []int, rules [][]int) bool {
-	for i := 0; i < len(seq)-1; i++ {
-		n0 := seq[i]
-		n1 := seq[i+1]
+func in_order(update []int, rules [][]int) bool {
+	for i := 0; i < len(update)-1; i++ {
+		n0 := update[i]
+		n1 := update[i+1]
 
 		for _, rule := range rules {
 			if slices.Contains(rule, n0) && slices.Contains(rule, n1) && rule[0] == n1 {
@@ -51,4 +57,22 @@ func in_order(seq []int, rules [][]int) bool {
 		}
 	}
 	return true
+}
+
+func order_update(update []int, rules [][]int) []int {
+	for ordered := false; !ordered; ordered = in_order(update, rules) {
+		for i := 0; i < len(update)-1; i++ {
+			n0 := update[i]
+			n1 := update[i+1]
+
+			for _, rule := range rules {
+				if slices.Contains(rule, n0) && slices.Contains(rule, n1) && rule[0] == n1 {
+					// flip n0 and n1
+					update = append(update[:i], update[i+1:]...)
+					update = slices.Insert(update, i+1, n0)
+				}
+			}
+		}
+	}
+	return update
 }
